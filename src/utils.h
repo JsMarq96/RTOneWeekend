60349ce09d7e30e4628b0ede9637dce1360cc22e
Juan S. Marquerie <jsmarquerie@gmail.com>
2024-11-13 15:07:20 +0100

    Update utils.h

--- src/utils.h
+++ src/utils.h
@@ -3,14 +3,22 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/random.hpp>
 
+#ifdef _WIN32
+#include <chrono>
+#else
 #include <sys/time.h>
+#endif
 
 inline double random_double() {
     return glm::linearRand(0.0, 1.0);
 }
 inline uint64_t get_timestamp_microsecs() {
+#ifdef _WIN32
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+#else
     struct timeval stamp;
     gettimeofday(&stamp, NULL);
 
     return stamp.tv_sec * 1000000u + stamp.tv_usec;
+#endif
 }
